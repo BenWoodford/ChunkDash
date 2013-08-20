7ea247862c5f13aed1c1e82dadce8bf971ae0088
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-20 14:28:45 +0100

    Fixed route

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@ Epi::init('api');
 
 getRoute()->get('/', 'home');
 getRoute()->get('/user/', array('User', 'stub'));
-getApi()->get('/user/(\[a-z0-9_\]{2,16})', array('User', 'getUser'), EpiApi::external);
+getApi()->get('/user/([a-z0-9_]{2,16})', array('User', 'getUser'), EpiApi::external);
 getApi()->get('/user/([a-z0-9_]{2,16})/regions', array('User', 'getRegions'), EpiApi::external);
 getApi()->get('/user/([a-z0-9_]{2,16})/regions/owned', array('User', 'getOwnedRegions'), EpiApi::external);
 getApi()->get('/user/([a-z0-9_]{2,16})/regions/member', array('User', 'getMembershipRegions'), EpiApi::external);
