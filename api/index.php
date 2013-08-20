f5ddfaf375a700e7657168a8de0373cbb7abbb9f
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-20 13:54:58 +0100

    Missing functions

--- api/index.php
+++ api/index.php
@@ -10,6 +10,9 @@ getRoute()->get('/user/#[a-z0-9_]{2,16}#i/regions', array('User', 'getRegions'))
 getRoute()->get('/user/#[a-z0-9_]{2,16}#i/regions/owned', array('User', 'getOwnedRegions'));
 getRoute()->get('/user/#[a-z0-9_]{2,16}#i/regions/member', array('User', 'getMembershipRegions'));
 
+function home() {
+	echo "Welcome to the API root.";
+}
 
 getRoute()->run();
 ?>
--- api/lib/handlers/User.php
+++ api/lib/handlers/User.php
@@ -2,6 +2,9 @@
 
 class User {
 
+	public static function stub() {
+		echo "Please use /api/user/<username>/";
+	}
 
 	public static function getOwnedRegions($user) {
 		return getDatabase()->all("SELECT * FROM `regions` WHERE FIND_IN_SET(:user, owners)", array(':user:', $user));
