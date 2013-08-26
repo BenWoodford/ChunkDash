1c09ace9b423716ddd80bad38cb2be29322197f5
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-26 14:47:41 +0100

    Added user clause

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ require_once("../global.php");
 Epi::init('template');
 Epi::setPath('view', 'skin/templates');
 
-getTemplate()->display('header.php', array('name' => $_SERVER['REMOTE_USER']));
+getTemplate()->display('header.php', array('name' => (isset($_SERVER['REMOTE_USER']) ? $_SERVER['REMOTE_USER'] : "anonymous@loadingchunks.net")));
 
 Epi::init('route');
 
