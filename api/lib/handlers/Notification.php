4cd2e87eda7bb7a424220656ad802437d2dc8c98
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-26 18:35:54 +0100

    Fixed parenths

--- api/lib/handlers/Notification.php
+++ api/lib/handlers/Notification.php
@@ -21,21 +21,19 @@ class Notification {
 	public static function getNotificationsMini($user = null) {
 		$notifications = getDatabase()->all("SELECT * FROM `notifications` ORDER BY `notification_id` DESC LIMIT 10");
 
-			foreach($notifications as $k=>$n) {
-				$notifications[$k]['ago'] = ago($n['time']);
+		foreach($notifications as $k=>$n) {
+			$notifications[$k]['ago'] = ago($n['time']);
 
-				if($user != null) {
-					$seen = getDatabase()->one("SELECT COUNT(*) as seen FROM `notifications_seen` WHERE `user` = :user LIMIT 1", array(':user' => $user));
-					if($seen['seen'] > 0) {
-						$notifications[$k]['seen'] = true;
-					} else {
-						$notifications[$k]['seen'] = false;
-						getDatabase()->execute("INSERT INTO `notifications_seen` (`notification_id`,`user`) VALUES (:id, :user)", array(':id' => $n['notification_id'], ':user' => $user));
-					}
+			if($user != null) {
+				$seen = getDatabase()->one("SELECT COUNT(*) as seen FROM `notifications_seen` WHERE `user` = :user LIMIT 1", array(':user' => $user));
+				if($seen['seen'] > 0) {
+					$notifications[$k]['seen'] = true;
+				} else {
+					$notifications[$k]['seen'] = false;
+					getDatabase()->execute("INSERT INTO `notifications_seen` (`notification_id`,`user`) VALUES (:id, :user)", array(':id' => $n['notification_id'], ':user' => $user));
 				}
 			}
 		}
-
 		return $notifications;
 	}
 }
