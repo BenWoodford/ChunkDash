0b87282c237641b5e240b6a21d97b4400a013b17
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-26 18:39:39 +0100

    Even prettier now

--- api/lib/functions.php
+++ api/lib/functions.php
@@ -1,24 +1,37 @@
 <?php
 function ago($time)
 {
-   $periods = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
-   $lengths = array("60","60","24","7","4.35","12","10");
+	$periods = array("sec", "min", "hour", "day", "week", "month", "year", "decade");
+	$lengths = array("60","60","24","7","4.35","12","10");
 
-   $now = time();
+	$now = time();
 
-       $difference     = $now - $time;
-       $tense         = "ago";
+	$difference = $now - $time;
+	$tense = "ago";
 
-   for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
-       $difference /= $lengths[$j];
-   }
+	for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+		$difference /= $lengths[$j];
+	}
 
-   $difference = round($difference);
+	$difference = round($difference);
 
-   if($difference != 1) {
-       $periods[$j].= "s";
-   }
+	if($difference == 1) {
+		switch($j) {
+			case 3:
+				return "yesterday";
+				break;
+			case 4:
+				return "last week";
+				break;
+			case 5:
+				return "last month";
+				break;
+			case 6:
+				return "last year";
+				break;
+		}
+	}
 
-   return "$difference $periods[$j] 'ago' ";
+	return "$difference $periods[$j]";
 }
 ?>
