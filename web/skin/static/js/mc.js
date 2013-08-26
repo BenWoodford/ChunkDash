9c624df491a06822bc369cfbf9d970d45ec69e48
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-26 20:13:11 +0100

    Changed classname

--- web/skin/static/js/mc.js
+++ web/skin/static/js/mc.js
@@ -1,13 +1,13 @@
 function getNotifications(firstPoll) {
 	$.getJSON('/api/notifications/mini/' + $("#logged_in").text(), function(json, textStatus) {
-		$("#notificationsList li.notification").remove();
+		$("#notificationsList li.noti").remove();
 		$.each(json, function(index, val) {
 			if(!val.seen && window.webkitNotifications) {
 				notification = window.webkitNotifications.createNotification("/skin/static/img/notificationicon.png", val.title, val.text);
 				notification.show();
 			}
 
-			$("#notificationsList li#view_all").before('<li class="notification" id="notification_' + val.notification_id + '"><a href="#">&nbsp;<i class="halflings-icon white comment"></i> <span class="message">' + val.title + '</span> <span class="time">' + val.ago + '</span></a></li>');
+			$("#notificationsList li#view_all").before('<li class="noti" id="notification_' + val.notification_id + '"><a href="#">&nbsp;<i class="halflings-icon white comment"></i> <span class="message">' + val.title + '</span> <span class="time">' + val.ago + '</span></a></li>');
 		});
 	});
 }
