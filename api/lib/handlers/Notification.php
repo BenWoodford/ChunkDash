ba69d325d3a7cc93b66fc9812dbec9d33e3f17ab
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-26 18:10:14 +0100

    Don't check notification_seen if user empty

--- api/lib/handlers/Notification.php
+++ api/lib/handlers/Notification.php
@@ -11,13 +11,15 @@ class Notification {
 	public static function getNotificationsMini($user = null) {
 		$notifications = getDatabase()->all("SELECT `notification_id`,`title`,`text`,`level`,`type` FROM `notifications` ORDER BY `notification_id` DESC LIMIT 10");
 
-		foreach($notifications as $k=>$n) {
-			$seen = getDatabase()->one("SELECT COUNT(*) as seen FROM `notifications_seen` WHERE `user` = :user LIMIT 1", array(':user' => $user));
-			if($seen['seen'] > 0) {
-				$notifications[$k]['seen'] = true;
-			} else {
-				$notifications[$k]['seen'] = false;
-				getDatabase()->execute("INSERT INTO `notifications_seen` (`notification_id`,`user`) VALUES (:id, :user)", array(':id' => $n['notification_id'], ':user' => $user));
+		if($user != null) {
+			foreach($notifications as $k=>$n) {
+				$seen = getDatabase()->one("SELECT COUNT(*) as seen FROM `notifications_seen` WHERE `user` = :user LIMIT 1", array(':user' => $user));
+				if($seen['seen'] > 0) {
+					$notifications[$k]['seen'] = true;
+				} else {
+					$notifications[$k]['seen'] = false;
+					getDatabase()->execute("INSERT INTO `notifications_seen` (`notification_id`,`user`) VALUES (:id, :user)", array(':id' => $n['notification_id'], ':user' => $user));
+				}
 			}
 		}
 
