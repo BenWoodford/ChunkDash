c4a7382cb0f0310e0f01092e32b57406c3e5075c
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-26 22:03:09 +0100

    Added Tasks content

--- web/index.php
+++ web/index.php
@@ -7,6 +7,7 @@ Epi::setPath('view', 'skin/templates');
 Epi::init('route');
 
 getRoute()->get('/', array('Overview', 'stub'));
+getRoute()->get('/tasks/?', array('Tasks', 'stub'));
 
 getRoute()->run();
 ?>
---
+++ web/lib/routes/Tasks.php
@@ -0,0 +1,13 @@
+<?php
+
+class Tasks extends Router {
+	public static function stub() {
+		parent::start();
+
+		
+		
+		parent::end();
+	}
+}
+
+?>
