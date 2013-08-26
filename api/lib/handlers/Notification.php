64e2751ce840428f9bb9ef0c32d6b1c5f1ebad06
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-26 17:49:32 +0100

    Those should be public

--- api/lib/handlers/Notification.php
+++ api/lib/handlers/Notification.php
@@ -1,14 +1,14 @@
 <?php
 
 class Notification {
-	static function getNotificationsFull($page = 0, $perpage = 25) {
+	public static function getNotificationsFull($page = 0, $perpage = 25) {
 		$page = @intval($page);
 		$perpage = @intval($perpage);
 
 		return getDatabase()->all("SELECT * FROM `notifications` ORDER BY `id` DESC LIMIT " . $page . "," . $perpage);
 	}
 
-	static function getNotificationsMini($user = null) {
+	public static function getNotificationsMini($user = null) {
 		$notifications = getDatabase()->all("SELECT `id`,`title`,`text`,`level`,`type` FROM `notifications` ORDER BY `id` DESC LIMTI 10");
 
 		foreach($notifications as $k=>$n) {
