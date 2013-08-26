3ba6b61a000fd0915eee115b595348cf709ccba5
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-26 15:36:39 +0100

    Redirect to root is user is already logged in

--- web/lib/routes/Login.php
+++ web/lib/routes/Login.php
@@ -1,7 +1,10 @@
 <?php
 class Login {
 	public static function stub() {
-		getTemplate()->display("login.php", array('referrer' => (isset($_GET['modopenid.referrer']) ? $_GET['modopenid.referrer'] : "http://dashboard.loadingchunks.net/")));
+		if(isset($_SERVER['REMOTE_USER']))
+			getRoute()->redirect('/', 307);
+		else
+			getTemplate()->display("login.php", array('referrer' => (isset($_GET['modopenid.referrer']) ? $_GET['modopenid.referrer'] : "http://dashboard.loadingchunks.net/")));
 	}
 }
 ?>
