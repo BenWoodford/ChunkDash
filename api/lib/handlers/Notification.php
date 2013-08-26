e5a44e5943a13bf725380c82ef3730bc4643bb7e
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-26 18:34:38 +0100

    Calculate clean time ago

--- api/lib/handlers/Notification.php
+++ api/lib/handlers/Notification.php
@@ -9,20 +9,29 @@ class Notification {
 		$page = @intval($page);
 		$perpage = @intval($perpage);
 
-		return getDatabase()->all("SELECT * FROM `notifications` ORDER BY `notification_id` DESC LIMIT " . $page . "," . $perpage);
+		$notifications = getDatabase()->all("SELECT * FROM `notifications` ORDER BY `notification_id` DESC LIMIT " . $page . "," . $perpage);
+
+		foreach($notifications as $k=>$n) {
+			$notifications[$k]['ago'] = ago($n['time']);
+		}
+
+		return $notifications;
 	}
 
 	public static function getNotificationsMini($user = null) {
-		$notifications = getDatabase()->all("SELECT `notification_id`,`title`,`text`,`level`,`type` FROM `notifications` ORDER BY `notification_id` DESC LIMIT 10");
+		$notifications = getDatabase()->all("SELECT * FROM `notifications` ORDER BY `notification_id` DESC LIMIT 10");
 
-		if($user != null) {
 			foreach($notifications as $k=>$n) {
-				$seen = getDatabase()->one("SELECT COUNT(*) as seen FROM `notifications_seen` WHERE `user` = :user LIMIT 1", array(':user' => $user));
-				if($seen['seen'] > 0) {
-					$notifications[$k]['seen'] = true;
-				} else {
-					$notifications[$k]['seen'] = false;
-					getDatabase()->execute("INSERT INTO `notifications_seen` (`notification_id`,`user`) VALUES (:id, :user)", array(':id' => $n['notification_id'], ':user' => $user));
+				$notifications[$k]['ago'] = ago($n['time']);
+
+				if($user != null) {
+					$seen = getDatabase()->one("SELECT COUNT(*) as seen FROM `notifications_seen` WHERE `user` = :user LIMIT 1", array(':user' => $user));
+					if($seen['seen'] > 0) {
+						$notifications[$k]['seen'] = true;
+					} else {
+						$notifications[$k]['seen'] = false;
+						getDatabase()->execute("INSERT INTO `notifications_seen` (`notification_id`,`user`) VALUES (:id, :user)", array(':id' => $n['notification_id'], ':user' => $user));
+					}
 				}
 			}
 		}
