33da538af67feed060fd6c4127bf3d7f878591e0
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-26 15:12:49 +0100

    Add optional to index.php instead

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@ Epi::setPath('view', 'skin/templates');
 
 Epi::init('route');
 
-getRoute()->get('/login', array('Login', 'stub'));
+getRoute()->get('/login/?', array('Login', 'stub'));
 
 getRoute()->run();
 ?>
