a1de5cbb207b2ba22786bf7fbd24e12ac4ef7c45
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-26 15:38:59 +0100

    Thorough check of REMOTE_USER

--- web/lib/routes/Login.php
+++ web/lib/routes/Login.php
@@ -1,7 +1,7 @@
 <?php
 class Login {
 	public static function stub() {
-		if(isset($_SERVER['REMOTE_USER']))
+		if(isset($_SERVER['REMOTE_USER']) && strlen($_SERVER['REMOTE_USER'] > 1))
 			getRoute()->redirect('/', 307);
 		else
 			getTemplate()->display("login.php", array('referrer' => (isset($_GET['modopenid.referrer']) ? $_GET['modopenid.referrer'] : "http://dashboard.loadingchunks.net/")));
