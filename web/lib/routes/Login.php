c7b27516284153d6e217b28bb20d1ecd725c947d
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-26 15:39:26 +0100

    Debug this pls

--- web/lib/routes/Login.php
+++ web/lib/routes/Login.php
@@ -2,7 +2,7 @@
 class Login {
 	public static function stub() {
 		if(isset($_SERVER['REMOTE_USER']) && strlen($_SERVER['REMOTE_USER'] > 1))
-			getRoute()->redirect('/', 307);
+			var_dump($_SERVER);
 		else
 			getTemplate()->display("login.php", array('referrer' => (isset($_GET['modopenid.referrer']) ? $_GET['modopenid.referrer'] : "http://dashboard.loadingchunks.net/")));
 	}
