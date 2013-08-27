0cae0931da5c3bfb317a6a3dfd98e35424131e62
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-27 08:58:27 +0100

    Added basic code for task adding

--- web/skin/static/js/pages/tasks/tasks.js
+++ web/skin/static/js/pages/tasks/tasks.js
@@ -1,15 +1,21 @@
 $.getJSON('/api/tasks/list', function(json, textStatus) {
 	$.each(json.tasks, function(index, val) {
 		var cat;
+		var pclass;
 
-		if(true) {
+		if(val.completed_by_id != null) {
 			if(val.starred) {
 				cat = $("#tasksHighPriority");
+				pclass = "high";
 			} else {
 				cat = $("#tasksNormalPriority");
+				pclass = "low";
 			}
 		} else {
 			cat = $("#tasksCompleted");
+			pclass = "medium";
 		}
+
+		cat.append('<div class="task ' + pclass + '"><div class="desc"><div class="title">' + val.title + '</div><div>' + val.note + '</div></div><div class="time"><div class="date">' + val.clean_date + '</div><div> ' + val.ago + '</div></div>');
 	});
 });
