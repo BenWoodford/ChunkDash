6672a3593b975a609cb1b0f32ae485a29840d2be
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-27 09:05:06 +0100

    Formatted times added

--- api/lib/handlers/Task.php
+++ api/lib/handlers/Task.php
@@ -29,6 +29,17 @@ class Task {
 
 		try {
 			$tasks = @Task::$wunderlist->getTasksByList(WLLIST, true);
+			$format = "F j, Y";
+			foreach($tasks as $k=>$t) {
+				$tasks[$k]['created_readable'] = (isset($t['created_at']) ? date($format, strtotime($t['created_at'])) : null);
+				$tasks[$k]['created_ago'] = (isset($t['created_at']) ? ago(strtotime($t['created_at'])) : null);
+
+				$tasks[$k]['due_readable'] = (isset($t['due_date']) ? date($format, strtotime($t['due_date'])) : null);
+				$tasks[$k]['deleted_readable'] = (isset($t['deleted_at']) ? date($format, strtotime($t['deleted_at'])) : null);
+				$tasks[$k]['completed_readable'] = (isset($t['completed_at']) ? date($format, strtotime($t['completed_at'])) : null);
+				$tasks[$k]['updated_readable'] = (isset($t['updated_at']) ? date($format, strtotime($t['updated_at'])) : null);
+			}
+
 			return $tasks;
 		} catch(Exception $e) {
 			return array(
