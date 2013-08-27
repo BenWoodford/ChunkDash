03399b4690904835bfef32450670184a54625ad4
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-27 08:45:41 +0100

    Maybe it's the wrong way around?

--- api/lib/handlers/Task.php
+++ api/lib/handlers/Task.php
@@ -28,7 +28,7 @@ class Task {
 			return $response;
 
 		try {
-			$tasks = @Task::$wunderlist->getTasksByList(WLLIST, true);
+			$tasks = @Task::$wunderlist->getTasksByList(WLLIST, false);
 			return $tasks;
 		} catch(Exception $e) {
 			return array(
--- web/skin/static/js/pages/tasks/tasks.js
+++ web/skin/static/js/pages/tasks/tasks.js
@@ -1,5 +1,5 @@
 $.getJSON('/api/tasks/list', function(json, textStatus) {
 	$.each(json.tasks, function(index, val) {
-		 console.log(val);
+		
 	});
 });
