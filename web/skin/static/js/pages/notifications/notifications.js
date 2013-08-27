ce459c936abda83d761c6e60b06df966da6bbf6a
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-27 20:14:38 +0100

    Old tasks stuff

--- web/skin/static/js/pages/notifications/notifications.js
+++ web/skin/static/js/pages/notifications/notifications.js
@@ -1,6 +1,6 @@
 function loadNotifications(page) {
 	$.getJSON('/api/notifications/full/' + page, function(json, textStatus) {
-		$.each(json.tasks, function(index, val) {
+		$.each(json, function(index, val) {
 			$("#messagesList").append('<li><span class="from"><span class="glypihons dislikes"><i></i></span> ' + val.type + '</span><span class="title">' + val.title + '</span><span class="date">' + val.ago + '</span></li>');
 		});
 	});
