d432c276ebf59dfef0f54091485c6b5edc98cb6a
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-27 18:06:10 +0100

    Subtasks yush

--- web/skin/static/js/pages/tasks/tasks.js
+++ web/skin/static/js/pages/tasks/tasks.js
@@ -26,6 +26,21 @@ $.getJSON('/api/tasks/list', function(json, textStatus) {
 		if(val.note == null)
 			val.note = "";
 
-		$(cat).append('<div id="task_' + val.id + '" data-task-id="' + val.id + '" class="task ' + pclass + ' row-fluid"><div class="desc span8"><div class="title"><a target="_blank" href="https://www.wunderlist.com/#/tasks/' + val.id + '">' + val.title + '</a></div><div>' + val.note + '</div></div><div class="time span4"><div class="date">' + due + '</div><div>created ' + val.created_ago + '</div></div>');
+		var subhtml = "";
+
+		$.each(val.subtasks, function(sindex, sval) {
+			 var tmp = "";
+
+			 if(sval.completed_by_id != null) {
+			 	tmp += "<strike>" + sval.title + "</strike>";
+			 }
+
+			 subhtml += tmp + ", ";
+		});
+
+		if(subhtml.charAt(subhtml.length - 1) == ',')
+			subhtml.slice(0, -1);
+
+		$(cat).append('<div id="task_' + val.id + '" data-task-id="' + val.id + '" class="task ' + pclass + ' row-fluid"><div class="desc span8"><div class="title"><a target="_blank" href="https://www.wunderlist.com/#/tasks/' + val.id + '">' + val.title + '</a></div><div><p>' + val.note + '</p><p><small>' + subhtml + '</small></p></div></div><div class="time span4"><div class="date">' + due + '</div><div>created ' + val.created_ago + '</div></div>');
 	});
 });
