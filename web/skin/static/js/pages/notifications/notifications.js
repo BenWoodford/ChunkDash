2feda43563382021ea9c1e9a7e7be7add70faa8c
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-28 09:32:32 +0100

    Move into oncomplete

--- web/skin/static/js/pages/notifications/notifications.js
+++ web/skin/static/js/pages/notifications/notifications.js
@@ -3,17 +3,17 @@ function loadNotifications(page) {
 		$.each(json, function(index, val) {
 			$("#notificationsList").append('<li class="notiBig" data-notification-id="' + val.notification_id + '"><span class="from"><span class="glypihons dislikes"><i></i></span> ' + val.type + '</span><span class="title">' + val.title + '</span><span class="date" title="' + val.cleantime + '">' + val.ago + '</span></li>');
 		});
-	});
 
-	$("#notificationsList .notiBig").click(function() {
-		$.getJSON('/api/notifications/single/' + $(this).data("notification-id"), function(json, textStatus) {
-			var view = $("#notificationVew");
+		$("#notificationsList .notiBig").click(function() {
+			$.getJSON('/api/notifications/single/' + $(this).data("notification-id"), function(json, textStatus) {
+				var view = $("#notificationVew");
 
-			$(view).find("#notificationTitle").text(json.title);
-			$(view).find("#notificationText").text(json.text);
-			$(view).find("#notificationTime").text(json.cleantime);
-			$(view).find("#notificationLevel").text(json.level);
-			$(view).find("#notificationType").text(json.type);
+				$(view).find("#notificationTitle").text(json.title);
+				$(view).find("#notificationText").text(json.text);
+				$(view).find("#notificationTime").text(json.cleantime);
+				$(view).find("#notificationLevel").text(json.level);
+				$(view).find("#notificationType").text(json.type);
+			});
 		});
 	});
 }
