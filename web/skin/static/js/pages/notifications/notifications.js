91dd325aff2f5b895c7526e88d5536ea3a3d4ce8
Ben Woodford <ben@woodfordnet.co.uk>
2013-08-28 09:30:43 +0100

    Fixed class

--- web/skin/static/js/pages/notifications/notifications.js
+++ web/skin/static/js/pages/notifications/notifications.js
@@ -1,7 +1,7 @@
 function loadNotifications(page) {
 	$.getJSON('/api/notifications/full/' + page, function(json, textStatus) {
 		$.each(json, function(index, val) {
-			$("#notificationsList").append('<li class="notification" data-notification-id="' + val.notification_id + '"><span class="from"><span class="glypihons dislikes"><i></i></span> ' + val.type + '</span><span class="title">' + val.title + '</span><span class="date" title="' + val.cleantime + '">' + val.ago + '</span></li>');
+			$("#notificationsList").append('<li class="notiBig" data-notification-id="' + val.notification_id + '"><span class="from"><span class="glypihons dislikes"><i></i></span> ' + val.type + '</span><span class="title">' + val.title + '</span><span class="date" title="' + val.cleantime + '">' + val.ago + '</span></li>');
 		});
 	});
 
