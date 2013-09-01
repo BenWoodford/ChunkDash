ff5fa880263a6234e17ce0b48205017b40c8a8cc
Ben Woodford <ben@woodfordnet.co.uk>
2013-09-01 15:13:07 +0100

    Oh, I see, it's 3

--- web/lib/routes/Metrics.php
+++ web/lib/routes/Metrics.php
@@ -4,7 +4,7 @@ class Metrics extends Router {
 	public static function stub() {
 		parent::start("metrics");
 
-		getTemplate()->display('metrics.php', array('today' => date("MM D, YYYY h:mm A"), 'yesterday' => date("MM D, YYYY h:mm A", strtotime("yesterday"))));
+		getTemplate()->display('metrics.php', array('today' => date("M D, Y h:mm A"), 'yesterday' => date("M D, Y h:mm A", strtotime("yesterday"))));
 
 		parent::end(array('/skin/static/js/pages/metrics/metrics.js'));
 	}
