3d17ef490a3943b59bcceae748c8fb5f31304680
Ben Woodford <ben@woodfordnet.co.uk>
2013-09-01 14:10:23 +0100

    Added time vars

--- web/lib/routes/Metrics.php
+++ web/lib/routes/Metrics.php
@@ -4,7 +4,7 @@ class Metrics extends Router {
 	public static function stub() {
 		parent::start("metrics");
 
-		getTemplate()->display('metrics.php');
+		getTemplate()->display('metrics.php', array('today' => date("d/m/Y"), 'yesterday' => date("d/m/Y", strtotime("yesterday"))));
 
 		parent::end();
 	}
