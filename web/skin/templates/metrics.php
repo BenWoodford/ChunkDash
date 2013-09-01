e34d9b7886d50379654d2f7effca123b5f80e01a
Ben Woodford <ben@woodfordnet.co.uk>
2013-09-01 14:13:50 +0100

    Formatted fields

--- web/skin/templates/metrics.php
+++ web/skin/templates/metrics.php
@@ -8,13 +8,15 @@
 	</div>
 </div>
 
-<div class="row-fluid">
-	<div class="span3">
-		<div class="control-group">
-			<label class="control-label">Date Range</label>
-			<div class="controls">
-				<input type="text" class="input-small datepicker" id="dateTo" value="<?=$yesterday;?>"> - <input type="text" class="input-small datepicker" id="dateFrom" value="<?=$today;?>">
+<form class="form-horizontal">
+	<div class="row-fluid">
+		<div class="span3">
+			<div class="control-group">
+				<label class="control-label">Date Range</label>
+				<div class="controls">
+					<input type="text" class="input-small datepicker" id="dateTo" value="<?=$yesterday;?>"> - <input type="text" class="input-small datepicker" id="dateFrom" value="<?=$today;?>">
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+</form>
