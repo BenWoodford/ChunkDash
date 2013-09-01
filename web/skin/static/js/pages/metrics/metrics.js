47f850797a684b2893ba5bbe019dac8636a7c6ab
Ben Woodford <ben@woodfordnet.co.uk>
2013-09-01 20:23:51 +0100

    getTime() pls

--- web/skin/static/js/pages/metrics/metrics.js
+++ web/skin/static/js/pages/metrics/metrics.js
@@ -48,8 +48,8 @@ $(document).ready(function() {
 							xaxis: {
 						   		mode: "time",
 						   		minTickSize: [1, data.axis.x.unit],
-						   		min: new Date(data.axis.x.min * 1000),
-						   		max: new Date(data.axis.x.max * 1000),
+						   		min: (new Date(data.axis.x.min * 1000)).getTime(),
+						   		max: (new Date(data.axis.x.max * 1000)).getTime(),
 							},
 							colors: data.colours,
 					}
